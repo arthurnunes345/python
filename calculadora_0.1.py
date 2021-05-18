2101be42ed5cf3a1fbc645e05014e526a49020e7
Arthur nunes <77701767+arthurnunes345@users.noreply.github.com>
2021-05-18 12:20:21 -0300

    calculadora

---
+++ calculadora_0.1.py
@@ -0,0 +1,7 @@
+num1 = int(input(" escolha um numero: "))
+num2 = int(input(" escolha um segundo numero"))
+quest = input(" escolha um calculo:")
+som4  = num1+ num2
+if quest=='soma':
+	print( som4)
+
